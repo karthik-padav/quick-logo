7606637742cba79931a69a800598b25240348672
karthikpadav <karthikpadav@gmail.com>
2024-06-03 23:16:46 +0530

    about page

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+import constants from "@/lib/constants";
+import Link from "next/link";
+
+export default function About() {
+  return <div className="text-black body-font">About</div>;
+}
